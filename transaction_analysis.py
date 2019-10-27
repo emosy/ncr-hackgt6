e430a8330e2c1e3f46ab81680da1837722160710
Ryan Thomas Lynch <hello@ryanlyn.ch>
2019-10-27 03:01:38 -0400

    added time condenser and updated imports

--- transaction_analysis.py
+++ transaction_analysis.py
@@ -1,6 +1,10 @@
 import json
 import http.client
 import plotly.graph_objects as go
+import datetime
+from datetime import timedelta, date, time
+from dateutil import parser
+import pytz
 # import plotly.express as px
 
 #requests a given page number of the data and returns just that page content
@@ -70,16 +74,42 @@ def count_shtuff(all_data):
         num += 1
     return num
 
-all_data = get_all_data(time="2019-10-24T05:00:00Z")
+#expects a list of lists, where the one at index has times for the corresponding other ones
+#data[0] and data[1] are both numbers that can be summed within the time delta given
+def combine_data_by_time(data, time_index, delta=timedelta(hours=1), indices={0,1}):
+    new_data = []
+    for d in data:
+        new_data.append([])
+    first_day = min(data[time_index])
+    first_day = parser.isoparse(first_day).date()
+    current_time_block_start = parser.isoparse(datetime.datetime.isoformat(datetime.datetime.combine(first_day,time.min))+"Z")
+    new_index = 0
+    i = 0
+    while i < len(data[time_index]):
+        for ind in indices:
+            new_data[ind].append(0)
+        new_data[time_index].append(datetime.datetime.isoformat(current_time_block_start))
+        while i < len(data[time_index]) and parser.isoparse(data[time_index][i]) < current_time_block_start + delta:
+            for ind in indices:
+                new_data[ind][-1] += data[ind][i]
+            i += 1
+        current_time_block_start += delta
+        new_index += 1
+    return new_data
+
+
+all_data = get_all_data(time="2019-10-20T00:00:00Z")
 num = count_shtuff(all_data)
 print(all_data[0])
 print(num)
 sorted_data_thing = collect_data_dict(all_data,['grandAmount','itemCount','transactionNumber','employeeIds','closeDateTimeUtc'])
 print(sorted_data_thing[4])
 # zip_data_thing = zip(sorted_data_thing)
-new_fig = go.Figure({"data": [{"type": "scatter", "mode": "markers", "x": list(range(len(sorted_data_thing[0]))), "y": sorted_data_thing[0], "marker.size": sorted_data_thing[1]}]})
+data_by_time = combine_data_by_time(sorted_data_thing,4)
+new_fig = go.Figure({"data": [{"type": "scatter", "mode": "markers",  "x": data_by_time[2], "y": data_by_time[0]}]})
 new_fig.write_html("my_html.html", auto_open=True)
 #, "hovertext": sorted_data_thing[4], "hoverinfo": "text"
+# "marker.size": sorted_data_thing[1], "mode": "markers",
 # for thingy in zip_data_thing:
 # new_graph = go.Bar(data=sorted_data_thing,hovertext=
 # new_fig = go.Figure(data=go.
