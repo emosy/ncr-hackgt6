7f645e45c6f3483e9abe71ca0df8730c0b0b9e8a
Ryan Thomas Lynch <hello@ryanlyn.ch>
2019-10-27 04:01:52 -0400

    my misadventure in python continutes
    somehow i fixed something by retyping it. thanks python

--- transaction_analysis.py
+++ transaction_analysis.py
@@ -78,7 +78,7 @@ def count_shtuff(all_data):
 #data[0] and data[1] are both numbers that can be summed within the time delta given
 def combine_data_by_time(data, time_index, delta=timedelta(hours=1), indices={0,1}):
     new_data = []
-    for d in data:
+    for d in range(len(indices)+1):
         new_data.append([])
     first_day = min(data[time_index])
     first_day = parser.isoparse(first_day).date()
@@ -88,25 +88,31 @@ def combine_data_by_time(data, time_index, delta=timedelta(hours=1), indices={0,
     while i < len(data[time_index]):
         for ind in indices:
             new_data[ind].append(0)
-        new_data[time_index].append(datetime.datetime.isoformat(current_time_block_start))
         while i < len(data[time_index]) and parser.isoparse(data[time_index][i]) < current_time_block_start + delta:
+            new_data[len(indices)].append(datetime.datetime.isoformat(current_time_block_start))
             for ind in indices:
                 new_data[ind][-1] += data[ind][i]
-            i += 1
+                i += 1
         current_time_block_start += delta
         new_index += 1
     return new_data
 
 
-all_data = get_all_data(time="2019-10-20T00:00:00Z")
+all_data = get_all_data(time="2019-10-23T12:00:00Z")
 num = count_shtuff(all_data)
 print(all_data[0])
 print(num)
 sorted_data_thing = collect_data_dict(all_data,['grandAmount','itemCount','transactionNumber','employeeIds','closeDateTimeUtc'])
-print(sorted_data_thing[4])
+print(sorted_data_thing[0])
 # zip_data_thing = zip(sorted_data_thing)
-data_by_time = combine_data_by_time(sorted_data_thing,4)
-new_fig = go.Figure({"data": [{"type": "scatter", "mode": "markers",  "x": data_by_time[2], "y": data_by_time[0]}]})
+data_by_time = combine_data_by_time(sorted_data_thing,4,delta=timedelta(minutes=5))
+print(data_by_time[2])
+max = max(data_by_time[0])
+print()
+print(max)
+print()
+print(data_by_time[2][-1])
+new_fig = go.Figure({"data": [{"type": "scatter", "mode": "markers",  "x": data_by_time[2], "y": data_by_time[0] }] })
 new_fig.write_html("my_html.html", auto_open=True)
 #, "hovertext": sorted_data_thing[4], "hoverinfo": "text"
 # "marker.size": sorted_data_thing[1], "mode": "markers",
